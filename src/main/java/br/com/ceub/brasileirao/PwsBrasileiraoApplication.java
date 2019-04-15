bcf5b384980bd6173e1dd7c60ff2bbbcc7bdd21f
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-04-15 14:10:41 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/ceub/brasileirao/PwsBrasileiraoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ceub.brasileirao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwsBrasileiraoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PwsBrasileiraoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ceub/brasileirao/PwsBrasileiraoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ceub.brasileirao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PwsBrasileiraoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
